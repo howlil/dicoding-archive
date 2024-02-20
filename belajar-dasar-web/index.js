b9a406e8aad53a51bc1c96b193c8768851447647
Mhd Ulil Abshar <87646428+howlil@users.noreply.github.com>
2024-02-21 04:28:26 +0700

    Delete belajar-dasar-web directory

--- belajar-dasar-web/index.js
+++
@@ -1,9 +0,0 @@
-const navmenu = document.querySelector(".nav-menu");
-const burger = document.querySelector(".burger");
-
-burger.addEventListener("click", mobile);
-function mobile ()  {
-  burger.classList.toggle("active");
-  navmenu.classList.toggle("active");
-
-};
